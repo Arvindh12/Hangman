9f9452f327ab0a4a87403fa125c8757bd08fbe85
Arvindh12 <arvindhashok1112@gmail.com>
2020-06-25 22:22:05 +0530

    game logic fully implemented

--- index.js
+++ index.js
@@ -10,6 +10,15 @@ var imglist = ["https://www.proprofs.com/games/word-games/hangman/image/First.pn
 var imgBox = document.getElementById("source")
 var charbox = document.getElementById("charbox")
 var life = document.getElementById("life")
+var guessbox = document.getElementById("guessbox")
+var displaytotalwords = document.getElementById("totalWordsguessed")
+function render(){
+    guessCount = 0;
+    life.innerText = `Lives Left: ${6-guessCount}`;
+    displaytotalwords.innerText = `Total words guessed: ${totalWordsguessed}`;
+    imgBox.src= "https://www.publicdomainpictures.net/pictures/30000/nahled/plain-white-background.jpg"
+    charbox.innerHTML = "";
+    guessbox.innerHTML = "";
 for (let i=65;i<65+26;i++){
     var div = document.createElement("div");
     div.setAttribute("class","char")
@@ -17,7 +26,7 @@ for (let i=65;i<65+26;i++){
     div.onclick =onclickhandle
     charbox.appendChild(div)
 }
-var guessbox = document.getElementById("guessbox")
+
 for (let i =0 ; i<word.length;i++){
     var div = document.createElement("div");
     div.setAttribute("class","guess")
@@ -25,6 +34,8 @@ for (let i =0 ; i<word.length;i++){
     guessbox.appendChild(div)
 
 }
+}
+render();
 function onclickhandle(e){
     var a = e.target.innerText;
     e.target.onclick = null;
@@ -53,6 +64,9 @@ function onclickhandle(e){
             ele.onclick = null;
         })
         //console.log(letters)
+        totalWordsguessed++;
+        document.getElementById("modalmessage").innerText = "You WIN!!!"
+    document.getElementById("id01").style.display='block'
     }
     if(guessCount == 6){
         console.log("gameover")
@@ -64,10 +78,18 @@ function onclickhandle(e){
         for(let i=0;i<word.length;i++){
         guessbox.children[i].innerText = word[i]       
     }
+    document.getElementById("modalmessage").innerText = "You LOST"
+    document.getElementById("id01").style.display='block'
 }
     if(guessCount>0){
     imgBox.src=imglist[guessCount-1]
     life.innerText = `Lives Left: ${6-guessCount}`
 }
     //console.log(imglist[guessCount-1])
+}
+
+function restartgame(){
+    document.getElementById('id01').style.display='none';
+    render();
+
 }
